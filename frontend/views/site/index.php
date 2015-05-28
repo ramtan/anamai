89fa928ca136c60000a7429db1adb54509e60c79
Kelpz <Kelpz@Non>
2015-05-28 23:25:38 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,41 @@
 <?php
+
+//ประกาศ NameSpance ไม่ต้องงเขียนคำสั่งยาว เหมือน extend
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 $this->title = 'กรมอนามัย';
 ?>
 
-sd4f654df6ds4f6
+<?php
+
+$route1 = Yii::$app->urlManager->createUrl("test/test1");
+
+?>
+
+
+
+<a href="<?= $route1 ?>">Go Test1</a>
+<br>
+
+
+<?php
+
+//create url แบบส่งค่าด้วย
+$route2 = Yii::$app->urlManager->createUrl(["test/test2","name"=>"Non","lname"=>"inwYii2"]);
+
+?>
+<a href="<?php echo  $route2 ;?>" >Go Test2</a>
+
+<br>
+
+<?=
+//create url อีกแบบ 
+Html::a("ลิ้งแบบที่ 3 ",["test/test2","name"=>"Daow","lname"=>"Daowja"]);
+?>
+
+<?php
+                        //create url แบบ 4
+    echo "<br><a href='".yii\helpers\Url::to(["test/test2","name"=>"Daoweng","lname"=>"Noneng"])."'>Link 4</a>";
+
+?>
